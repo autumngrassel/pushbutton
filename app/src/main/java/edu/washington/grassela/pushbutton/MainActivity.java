cc524a0cb7fa6738978b43b3665e3a6ebf77e5e5
Autumn Grassel <grassela@uw.edu>
2015-01-20 15:00:54 -0800

    first commit

---
+++ app/src/main/java/edu/washington/grassela/pushbutton/MainActivity.java
@@ -0,0 +1,49 @@
+package edu.washington.grassela.pushbutton;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+    private int timesPushed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        timesPushed = 0;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void buttonPushed(View view) {
+        Button button = (Button) findViewById(R.id.button);
+        timesPushed++;
+        button.setText("You have pushed me " + timesPushed + " times!");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
